9ba56d5050206240665de1b19b8d0975438041a7
Nitin Jaladanki <nitin.jaladanki@gmail.com>
2019-07-18 17:32:02 -0700

    Initial Commit

---
+++ Trivia V2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Trivia V2
+//
+//  Created by Chicken Nugget on 2019. 07. 18..
+//  Copyright © 2019. Nitin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
